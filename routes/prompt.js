18bdd28f2e89612f95a379ee966f6a576ef2cc93
Joe Lee <joelee@stanford.edu>
2014-05-22 07:56:09 -0700

    crappy game log

--- routes/prompt.js
+++ routes/prompt.js
@@ -9,15 +9,10 @@ exports.view = function(req, res){
 
   var currUser = user_data.get_user_by_id(req.session.curr_user_id);
 
-  var isJoe = undefined;
-  if (currUser["first_name"] == "Joe") {
-  	isJoe = true;
-  }
-
   res.render('prompt', 
   {
   	'user' : currUser,
-  	'isJoe' : isJoe
+    'photo_arr' : photosdata.photos
   });
 
 };
